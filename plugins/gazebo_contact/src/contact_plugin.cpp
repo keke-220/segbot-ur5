82b9a447a9288d267f7568fc8c38a144ef6c89ef
keke-220 <xzhan244@binghamton.edu>
2023-08-28 03:32:11 +0000

    add third-party support for wiping

---
+++ plugins/gazebo_contact/src/contact_plugin.cpp
@@ -0,0 +1,72 @@
+#include "contact_plugin.hh"
+#include <ros/ros.h>
+#include <fstream>
+using namespace gazebo;
+using namespace std;
+GZ_REGISTER_SENSOR_PLUGIN(ContactPlugin)
+
+/////////////////////////////////////////////////
+ContactPlugin::ContactPlugin() : SensorPlugin()
+{
+}
+
+/////////////////////////////////////////////////
+ContactPlugin::~ContactPlugin()
+{
+}
+
+/////////////////////////////////////////////////
+void ContactPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr /*_sdf*/)
+{
+  // Get the parent sensor.
+  this->parentSensor =
+    std::dynamic_pointer_cast<sensors::ContactSensor>(_sensor);
+
+  // Make sure the parent sensor is valid.
+  if (!this->parentSensor)
+  {
+    gzerr << "ContactPlugin requires a ContactSensor.\n";
+    return;
+  }
+  ROS_INFO("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+  // Connect to the sensor update event.
+  this->updateConnection = this->parentSensor->ConnectUpdated(
+      std::bind(&ContactPlugin::OnUpdate, this));
+
+  // Make sure the parent sensor is active.
+  this->parentSensor->SetActive(true);
+}
+
+/////////////////////////////////////////////////
+void ContactPlugin::OnUpdate()
+{
+  // Get all the contacts.
+  msgs::Contacts contacts;
+  contacts = this->parentSensor->Contacts();
+  for (unsigned int i = 0; i < contacts.contact_size(); ++i)
+  {
+    std::cout << "Collision between[" << contacts.contact(i).collision1()
+              << "] and [" << contacts.contact(i).collision2() << "]\n";
+
+    for (unsigned int j = 0; j < contacts.contact(i).position_size(); ++j)
+    {
+      std::cout << j << "  Position:"
+                << contacts.contact(i).position(j).x() << " "
+                << contacts.contact(i).position(j).y() << " "
+                << contacts.contact(i).position(j).z() << "\n";
+      std::cout << "   Normal:"
+                << contacts.contact(i).normal(j).x() << " "
+                << contacts.contact(i).normal(j).y() << " "
+                << contacts.contact(i).normal(j).z() << "\n";
+      std::cout << "   Depth:" << contacts.contact(i).depth(j) << "\n";
+      ofstream myfile;
+      myfile.open ("temp_contact.txt", ios::out | ios::app);
+      myfile << contacts.contact(i).position(j).x() << " "
+             << contacts.contact(i).position(j).y() << " "
+             << contacts.contact(i).position(j).z() << "\n";
+      myfile.close();
+      std::cout << "Write to file done\n";
+    }
+  }
+}
